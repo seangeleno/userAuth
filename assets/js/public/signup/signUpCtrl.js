52cfe00ef61152a7ef77b18175fd051f5e6a431e
Sean Esteva <seangeleno@gmail.com>
2016-01-30 10:10:48 -0800

    added runSignup function to controller and ng-model to name email and password

--- assets/js/public/signup/signUpCtrl.js
+++ assets/js/public/signup/signUpCtrl.js
@@ -1,3 +1,7 @@
 angular.module('SignupMod').controller('SignupCtrl',['$scope', function($scope){
   console.log('Signup Controller Initialized');
+
+  $scope.runSignup = function(){
+    console.log('Sign up'+$scope.name);
+  }
 }])
