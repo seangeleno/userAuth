b3aac9643a61cd56ac1817b6e6ac9ef20357f615
Sean Esteva <seangeleno@gmail.com>
2016-01-30 11:49:03 -0800

    fixed bugs, can add user now

--- api/models/User.js
+++ api/models/User.js
@@ -8,27 +8,26 @@
 module.exports = {
 
   attributes: {
-    name: {
-      type: 'string',
-      required: true
-    },
-    email:{
-      type:'string',
-      email: true,
-      required: true,
-      unique: true
-    },
-    Password: {
-        type:'string',
-        required: true
-    },
-    lastLoggedIn: {
-      type: 'date',
-      required: true,
-      defaultsTo: new Date(0)
-    },
-    gravatarUrl: {
-      type: 'string'
-    }
+  	name: {
+  		type:'string',
+  		required: true
+  	},
+  	email:{
+  		type:'string',
+  		required: true,
+  		unique: true
+  	},
+  	password:{
+  		type:'string',
+  		required: true
+  	},
+  	lastLoggedIn:{
+  		type:'date',
+  		required: true,
+  		defaultsTo: new Date(0)
+  	},
+  	gravatarUrl:{
+  		type:'string'
+  	}
   }
 };
--- assets/js/public/signup/signUpCtrl.js
+++ assets/js/public/signup/signUpCtrl.js
@@ -1,23 +1,20 @@
 angular.module('SignupMod').controller('SignupCtrl',['$scope', '$http', function($scope, $http){
-  console.log('Signup Controller Initialized');
+	console.log('Signup Controller initialized...');
 
-  $scope.runSignup = function(){
-    console.log('Sign up' + $scope.name);
+	$scope.runSignup = function(){
+		console.log('Signing Up '+$scope.name);
 
-    //Submit to Sails Server via post request
-    $http.post('/signup', {
-      name: $scope.name,
-      email: $scope.email,
-      password: $scope.passwords
-    })
-    .then(function onSuccess(response){
-      window.location = '/user'
-    })
-    .catch(function onError(err){
-      console.log('Error: '+err);
-    })
-    // .finally(function either(){
-    //
-    // })
-  }
+		// Submit To Sails Server
+		$http.post('/signup', {
+			name: $scope.name,
+			email: $scope.email,
+			password: $scope.password
+		})
+		.then(function onSuccess(response){
+			window.location = '/user'
+		})
+		.catch(function onError(err){
+			console.log('Error: ',err);
+		})
+	}
 }])
